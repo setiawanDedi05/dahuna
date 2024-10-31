ebde7f1329881b03c24f524fd1ad00243a6b4520
setiawanDedi05 <dssetiawan05@gmail.com>
2024-10-31 20:16:10 +0700

    add logo in clark ui

--- providers/index.tsx
+++ providers/index.tsx
@@ -4,17 +4,23 @@ import { ClerkProvider } from "@clerk/nextjs";
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <ClerkProvider appearance={{
-        layout: {
-          socialButtonsPlacement: "bottom",
-          socialButtonsVariant: "blockButton",
-        } 
-      }}
-      afterSignOutUrl="/sign-in"
-      afterSignInUrl="/dashboard"
-      signInFallbackRedirectUrl="/sign-in"
-      signUpFallbackRedirectUrl="/"
-      >{children}</ClerkProvider>
+      <ClerkProvider
+        appearance={{
+          layout: {
+            logoImageUrl: "/assets/images/logo.png",
+            socialButtonsPlacement: "bottom",
+            socialButtonsVariant: "blockButton",
+            logoPlacement: "inside",
+            shimmer: true,
+          },
+        }}
+        afterSignOutUrl="/sign-in"
+        afterSignInUrl="/dashboard"
+        signInFallbackRedirectUrl="/sign-in"
+        signUpFallbackRedirectUrl="/"
+      >
+        {children}
+      </ClerkProvider>
     </div>
   );
 };
