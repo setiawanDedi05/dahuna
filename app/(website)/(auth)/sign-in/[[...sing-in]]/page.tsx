0f1187f62ba697505a4460ca267749d5b66f6961
setiawanDedi05 <dssetiawan05@gmail.com>
2024-10-31 20:11:58 +0700

    enhancement design signin and signup

--- app/(website)/(auth)/sign-in/[[...sing-in]]/page.tsx
+++ app/(website)/(auth)/sign-in/[[...sing-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return <>
+    <div className="hidden md:flex bg-cover bg-center bg-[url('/assets/images/image-4.jpg')]" />
+    <div className="flex justify-center items-center">
+      <SignIn />
+    </div>
+  </>;
 }
--- app/(website)/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(website)/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,12 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <>
+      <div className="hidden md:flex bg-cover bg-center bg-[url('/assets/images/image-5.jpg')]" />
+      <div className="flex justify-center items-center">
+        <SignUp />
+      </div>
+    </>
+  );
 }
