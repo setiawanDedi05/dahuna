31766a63321ab01e11b488f8dd3af48aabfc674f
setiawanDedi05 <dssetiawan05@gmail.com>
2024-11-01 13:53:19 +0700

    fill icon group

--- components/custom/IconGroup.tsx
+++ components/custom/IconGroup.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { SearchIcon, ShoppingCart, User, X } from "lucide-react";
+import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "../ui/input";
 
 export const IconGroup = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>IconGroup</div>
-  )
-}
+    <section className="flex gap-5 items-center">
+      <Dialog open={open}>
+        <DialogTrigger
+          className="cursor-pointer"
+          onClick={() => setOpen(true)}
+        >
+          <SearchIcon size={24} />
+        </DialogTrigger>
+        <DialogContent className="[&>.closeBtn]:hidden rounded-md">
+          <div className="flex justify-center items-center gap-5">
+            <SearchIcon className="text-slate-300" />
+            <Input placeholder="search product" />
+            <Button
+              variant="nostyle"
+              onClick={() => setOpen(false)}
+              className="hover:bg-red-400 !p-3 hover:text-white"
+            >
+              <X />
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+      <Button variant="nostyle" size="icon" className="hidden lg:flex">
+        <ShoppingCart size={32} />
+      </Button>
+      <Button variant="nostyle" size="icon" className="hidden lg:flex">
+        <User size={32} />
+      </Button>
+    </section>
+  );
+};
