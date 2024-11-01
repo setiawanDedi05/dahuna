cecdc09776a387f1979faa4de779255fef2f4e6a
setiawanDedi05 <dssetiawan05@gmail.com>
2024-11-01 13:53:40 +0700

    finishing logo component

--- components/custom/Logo.tsx
+++ components/custom/Logo.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export const Logo = () => {
   return (
-    <div>Logo</div>
-  )
-}
+    <Link href="/" className="flex justify-center items-center gap-1">
+      <Image
+        src="/assets/images/logo.png"
+        alt="logo"
+        width={50}
+        height={50}
+        className="object-contain border-r-2"
+      />
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-md lg:text-2xl">Dahuna</h2>
+        <h5 className="text-primary-foreground text-xs bg-primary w-full text-right px-1">Clothing</h5>
+      </div>
+    </Link>
+  );
+};
