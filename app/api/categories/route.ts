35ad682a6d58a30e2d420ab3f62e7fb7504718ff
setiawanDedi05 <dssetiawan05@gmail.com>
2024-11-03 21:14:40 +0700

    add api category

---
+++ app/api/categories/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+  return NextResponse.json({
+    content: [
+      {
+        _id: "1",
+        title: "Woman Cloth",
+        link: "/products?caregory=woman-cloth",
+        image: "/assets/images/woman-cloth.png",
+      },
+      {
+        _id: "2",
+        title: "Man Cloth",
+        link: "/products?caregory=man-cloth",
+        image: "/assets/images/man-cloth.png",
+      },
+      {
+        _id: "3",
+        title: "Boy Cloth",
+        link: "/products?caregory=boy-cloth",
+        image: "/assets/images/boy-cloth.png",
+      },
+      {
+        _id: "3",
+        title: "Girl Cloth",
+        link: "/products?caregory=girl-cloth",
+        image: "/assets/images/girl-cloth.png",
+      },
+      {
+        _id: "4",
+        title: "Baby Boy Cloth",
+        link: "/products?caregory=baby-boy-cloth",
+        image: "/assets/images/baby-boy-cloth.png",
+      },
+    ],
+  });
+};
