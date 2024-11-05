41037713b6fbd19fa903b19dfd59709f1cc27397
setiawanDedi05 <dssetiawan05@gmail.com>
2024-11-05 21:45:04 +0700

    add animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 20s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(-100%)",
+          },
+        },
+      },
       container: {
         center: true,
         screens: {
