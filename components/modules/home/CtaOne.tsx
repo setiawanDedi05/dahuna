dc0183dbe10d7354d1a5a8be31a35c05bcdf21cc
setiawanDedi05 <dssetiawan05@gmail.com>
2024-11-05 21:47:58 +0700

    delete section CTA

--- components/modules/home/CtaOne.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CtaOne = () => {
-  return (
-    <div>CtaOne</div>
-  )
-}
--- components/modules/home/CtaTwo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CtaTwo = () => {
-  return (
-    <div>CtaTwo</div>
-  )
-}
