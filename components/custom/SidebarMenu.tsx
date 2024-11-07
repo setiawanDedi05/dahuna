b493fbd23dd1f3fe94d06586c08f310bfb20cc62
setiawanDedi05 <dssetiawan05@gmail.com>
2024-11-07 21:39:26 +0700

    change side bar menu

--- components/custom/SidebarMenu.tsx
+++ components/custom/SidebarMenu.tsx
@@ -4,14 +4,16 @@ import React from "react";
 import {
   Sheet,
   SheetContent,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
 export const SidebarMenu = () => {
+  const pathname = usePathname();
   return (
     <Sheet>
       <SheetTrigger>
@@ -21,21 +23,17 @@ export const SidebarMenu = () => {
         side="left"
         className={cn("px-4 w-full [&>#closeBtn]:text-4xl", "md:w-[400px]")}
       >
-        <SheetTitle>Categories</SheetTitle>
-        <div className="grid grid-cols-3 gap-y-10 mt-10">
-          <Link href="/" className="text-left">
-            Woman Cloth
-          </Link>
-          <Link href="/" className="text-left">
-            Man Cloth
-          </Link>
-          <Link href="/" className="text-left">
-            Boy Cloth
-          </Link>
-          <Link href="/" className="text-left">
-            Girl Cloth
-          </Link>
-        </div>
+        <nav className="flex flex-col mt-10 gap-y-5">
+          <Button asChild variant={pathname === "/" ? "default" : "outline"}>
+            <Link href="/">Home</Link>
+          </Button>
+          <Button
+            asChild
+            variant={pathname === "/products" ? "default" : "outline"}
+          >
+            <Link href="/products">Store</Link>
+          </Button>
+        </nav>
       </SheetContent>
     </Sheet>
   );
