31585bd5f43200a452dd10545e6646504da90fd2
setiawanDedi05 <dssetiawan05@gmail.com>
2024-11-21 17:02:08 +0700

    add tooltip in mobile bottom bar

--- components/modules/header/MobileButton.tsx
+++ components/modules/header/MobileButton.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import { Boxes, History, HistoryIcon, Home } from "lucide-react";
 import Link from "next/link";
@@ -9,57 +14,72 @@ import React from "react";
 export const MobileButton = () => {
   const pathname = usePathname();
   return (
-    <div className="fixed md:hidden z-50 bottom-10 w-full h-[80px] ">
-      <div className="w-3/4 h-full bg-primary/70 mx-auto rounded-lg flex text-primary-foreground items-center">
-        <Link
-          href="/"
-          className={cn(
-            pathname === "/" && "border-b-4",
-            "h-full w-1/3 flex justify-center items-center cursor-pointer"
-          )}
-        >
-          <Home
-            size={32}
+    <div className="fixed md:hidden z-50 bottom-10 w-full h-[40px] ">
+      <div className="h-full w-[300px] bg-primary mx-auto rounded-lg flex text-primary-foreground items-center">
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger
             className={cn(
-              pathname === "/"
-                ? ""
-                : "hover:rotate-12 hover:duration-1000",
-              "p-3 size-16"
+              pathname === "/" && "border-b-4",
+              "h-full w-1/3 flex justify-center items-center cursor-pointer"
             )}
-          />
-        </Link>
-        <Link
-          href="/products"
-          className={cn(
-            pathname === "/products" && "border-b-4",
-            "h-full w-1/3 flex justify-center items-center cursor-pointer"
-          )}
-        >
-          <Boxes
+          >
+            <Link href="/">
+              <Home
+                className={cn(
+                  pathname === "/" ? "" : "hover:rotate-12 hover:duration-1000",
+                  "p-3 size-10"
+                )}
+              />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Home</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger
             className={cn(
-              pathname === "/products"
-                ? ""
-                : "hover:rotate-12 hover:duration-1000",
-              "p-3 size-16"
+              pathname === "/products" && "border-b-4",
+              "h-full w-1/3 flex justify-center items-center cursor-pointer"
             )}
-          />
-        </Link>
-        <Link
-          href="/histories"
-          className={cn(
-            pathname === "/histories" && "border-b-4",
-            "h-full w-1/3 flex justify-center items-center cursor-pointer"
-          )}
-        >
-          <HistoryIcon
+          >
+            <Link href="/products">
+              <Boxes
+                className={cn(
+                  pathname === "/products"
+                    ? ""
+                    : "hover:rotate-12 hover:duration-1000",
+                  "p-3 size-10"
+                )}
+              />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Store</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger
             className={cn(
-              pathname === "/histories"
-                ? ""
-                : "hover:rotate-12 hover:duration-1000",
-              "p-3 size-16"
+              pathname === "/histories" && "border-b-4",
+              "h-full w-1/3 flex justify-center items-center cursor-pointer"
             )}
-          />
-        </Link>
+          >
+            <Link href="/histories">
+              <HistoryIcon
+                className={cn(
+                  pathname === "/histories"
+                    ? ""
+                    : "hover:rotate-12 hover:duration-1000",
+                  "p-3 size-10"
+                )}
+              />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>History</p>
+          </TooltipContent>
+        </Tooltip>
       </div>
     </div>
   );
